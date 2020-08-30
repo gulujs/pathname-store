63166e374e9c62839c45ec2e550e8f7c20739c6e
jKey Lu <jkeylu@gmail.com>
2020-08-30 02:08:22 +0000

    style: format types file, and add some comment

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,18 @@ export interface PathnameStoreOptions {
     backtrack?: boolean;
     caseSensitive?: boolean;
     paramNamePattern?: string;
+    /**
+     * @param box Structured object that previously returned
+     * @param store
+     * @param pnames
+     * @returns A new customized object
+     */
     boxing?: (box: any, store: any, pnames: string[]) => any;
 }
 
 export class PathnameStore {
-  constructor(options?: PathnameStoreOptions);
-  add(path: string, store: any): void;
-  find(path: string): { found: false; } | { found: true; pvalues: string[]; box: any; };
-  prettyPrint(options: any): string;
+    constructor(options?: PathnameStoreOptions);
+    add(path: string, store: any): void;
+    find(path: string): { found: false; } | { found: true; pvalues: string[]; box: any; };
+    prettyPrint(options: any): string;
 }
