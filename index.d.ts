b9ec99d659b87f502e7502437f1a0b60dc3bb8e4
jKey Lu <jkeylu@gmail.com>
2022-05-26 21:55:12 +0800

    types: update type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 import { DrawOptions } from '@gulujs/archy';
 
+export type BoxingFn = (box: any, store: any, pnames: string[]) => any;
+
 export interface PathnameStoreOptions {
   backtrack?: boolean;
   caseSensitive?: boolean;
@@ -10,13 +12,19 @@ export interface PathnameStoreOptions {
    * @param pnames
    * @returns A new customized object
    */
-  boxing?: (box: unknown, store: unknown, pnames: string[]) => unknown;
+  boxing?: BoxingFn;
 }
 
 export declare class PathnameStore {
+  backtrack: boolean;
+  caseSensitive: boolean;
+  paramNamePattern: string;
+  paramNameRE: RegExp;
+  boxing?: BoxingFn | null;
+
   constructor(options?: PathnameStoreOptions);
-  add(path: string, store: unknown): void;
-  find(path: string): { found: false; } | { found: true; pvalues: string[]; box: unknown; };
+  add(path: string, store: any): void;
+  find<T = unknown>(path: string): { found: false; } | { found: true; pvalues: string[]; box: T; };
   reset(): void;
   prettyPrint(options?: DrawOptions): string;
 }
